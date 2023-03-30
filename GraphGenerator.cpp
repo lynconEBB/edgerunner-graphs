554659d82555e0f9e6a476a9a3620e34a05958bc
Lyncon Baez <lynconlyn@gmail.com>
2023-03-30 09:57:32 -0300

    First commit

---
+++ GraphGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 30/03/2023.
+//
+
+#include "GraphGenerator.h"
---
+++ GraphGenerator.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHS_GRAPHGENERATOR_H
+#define GRAPHS_GRAPHGENERATOR_H
+
+
+class GraphGenerator {
+public:
+
+};
+
+
+#endif
