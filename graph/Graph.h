e21352488d01f06391b24d016fc7fd9ce60d170b
tomiodarim <lucastomiodarim@gmail.com>
2023-03-31 09:53:11 -0300

    Changes by user

--- graph/Graph.h
+++ graph/Graph.h
@@ -11,6 +11,10 @@ public:
     int32_t weight;
 
     Edge(int32_t inDest, int32_t inWeight) : dest(inDest), weight(inWeight) {}
+
+    friend bool operator> ( const Edge& edge1, const Edge& edge2 ) {
+        return edge1.weight > edge2.weight;
+    }
 };
 
 class Graph {
@@ -26,6 +30,7 @@ public:
     void addEdge(int32_t src, int32_t dest, int32_t weight);
 
     void print();
+
 };
 
 #endif
